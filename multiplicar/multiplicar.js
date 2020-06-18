36dd18fd4d47d89ed2130c55927f2e1b50e22932
Ricardo Granja <chirica2501@hotmail.com>
2020-06-18 15:49:00 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+    
+    console.log('===================='.green);
+    console.log(`==== Tabla de ${base} ====`.red);
+    console.log('===================='.green);
+    
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${ base } * ${ i } = ${ base * i }`);
+    }
+}
+
+let crearArchivo = (base, limite = 10) => {
+    return new Promise( (resolve, reject) => {
+        let data = '';
+
+        if (!Number(base) && !Number(limite)) {
+            reject('El valor o limite introducido no es un número');
+            return;
+        }
+        for (let i = 1; i <= limite; i++) {
+            data += `${ base } * ${ i } = ${ base * i }\n`;
+        }
+        
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+          if (err)
+            reject(err);
+          else
+            resolve(`tabla-${base}.txt`);
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo, listarTabla
+}
